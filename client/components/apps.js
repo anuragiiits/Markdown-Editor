79a85018963f90952ac0703e4253544777f49385
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-25 10:10:44 +0530

    Publication and Subscriptionof Bins done

--- client/components/apps.js
+++ client/components/apps.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header';
+import BinsList from '../components/bins/bins_list'
 
 export default () =>{
     return(
             <div>
                 <Header />
+                <BinsList />
             </div>
     );
 };
