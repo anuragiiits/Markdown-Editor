62fc21ac51affc3b1aa3c1999cffae7a7f63f775
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-27 00:50:12 +0530

    Input and Output of editor done

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import {Bins} from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends Component{
     render(){
@@ -13,6 +14,7 @@ class BinsMain extends Component{
         return(
             <div> 
                 <BinsEditor bin={this.props.bin} />
+                <BinsViewer bin={this.props.bin} />
             </div>
         );
     };
