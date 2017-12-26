b1481baa15a97e9c8dd1ece1f252a405166849a2
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-26 21:12:37 +0530

    Markdown editor created

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import {Bins} from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
 
 class BinsMain extends Component{
     render(){
         //console.log(this.props.match.params.binId);
+        //console.log(this.props.bin);
         return(
-            <div> Bins Main</div>
+            <div> 
+                <BinsEditor bin={this.props.bin} />
+            </div>
         );
     };
 }
 
-export default BinsMain;
+export default withTracker((props)=>{
+    Meteor.subscribe('bins');
+    const {binId} = props.match.params;
+
+    return {bin: Bins.findOne(binId)}
+})(BinsMain);
