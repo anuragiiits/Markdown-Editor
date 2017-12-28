500023f68c18252fe90dd953fef36412435235b4
Anurag Gupta <anurag.iiits@gmail.com>
2017-12-28 14:36:00 +0530

    Bugs fixes and warning removed

--- client/components/header.js
+++ client/components/header.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react';
 import Account from './accounts';
 import {Link} from 'react-router-dom';
 
+
 class Header extends Component{
 
     onBinsClicked(event){
         event.preventDefault();
-
+        //console.log(Meteor.userId());
+        //if(Meteor.userId() != null){  //to remove the feature of anonymous user
         Meteor.call('bins.insert',(error, binId)=>{
             //console.log(this.props.history);//.push(`bins/${binId}`);
             this.props.history.push(`/bins/${binId}`);
         });
+        //}
+        
     }
     
     render(){
--- server/main.js
+++ server/main.js
@@ -3,13 +3,15 @@ import {Bins} from '../imports/collections/bins';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  
   Meteor.publish('bins',function(){
+    //if(this.userId == null) {return; } removing this line will add a feature of using app anonymously
     return Bins.find({ownerId: this.userId});
   });
 
   Meteor.publish('sharedBins', function(){
     const user = Meteor.users.findOne(this.userId);
-
+    //console.log(this.userId);
     if(!user) { return; }
 
     const email = user.emails[0].address;
